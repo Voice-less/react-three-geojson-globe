ee670c39691af4f94ee94600e09cb8c4933fce17
Voice-less <lamzib123@gmail.com>
2025-08-27 01:00:43 +0100

    deploying setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-three-geojson-globe/', 
   plugins: [react()],
   server: {
     watch: {
